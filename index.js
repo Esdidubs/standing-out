00b790d6f28c0816fe104783e173a20ed5f5024b
Esdidubs <sward3644@gmail.com>
2021-02-12 09:41:57 -0700

    Fixed indents & spaces, changed to single quotes

--- index.js
+++ index.js
@@ -3,60 +3,58 @@ let cardNum = 0;
 
 // Fisher-Yates shuffle
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    shuffledArray = array;
-    return shuffledArray;
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		[ array[i], array[j] ] = [ array[j], array[i] ];
+	}
+	shuffledArray = array;
+	return shuffledArray;
 }
 
-function getCard(){
-    if(cardNum >= 52){
-        cardNum =0;
-    }
-    let setCard = shuffledArray[cardNum];
-    console.log(cardNum)
-    displayCard(setCard);
-    cardNum++;
+function getCard() {
+	if (cardNum >= 52) {
+		cardNum = 0;
+	}
+	let setCard = shuffledArray[cardNum];
+	console.log(cardNum);
+	displayCard(setCard);
+	cardNum++;
 }
 
-function displayCard(card){
-    $('#cardSection').replaceWith(
-        `<div id="cardSection">
+function displayCard(card) {
+	$('#cardSection').replaceWith(
+		`<div id="cardSection">
             <img src=${card} alt="Standing Out Card" class="roundCard" />
         </div>`
-      )
+	);
 }
 
 // Events for all of the buttons
 function buttons() {
-    $('#cardForm').on( "click", "#newGame", function() {
-      event.preventDefault();
-      getCard();
-      if(!($('#rules').hasClass('hidden'))){
-        $('#rules').addClass('hidden');
-      };
-      $('#newGame').addClass('hidden');
-      $('#newCard').removeClass('hidden');
-      $('#btnRules').removeClass('hidden');
-    });
+	$('#cardForm').on('click', '#newGame', function() {
+		event.preventDefault();
+		getCard();
+		if (!$('#rules').hasClass('hidden')) {
+			$('#rules').addClass('hidden');
+		}
+		$('#newGame').addClass('hidden');
+		$('#newCard').removeClass('hidden');
+		$('#btnRules').removeClass('hidden');
+	});
 
-    $('#cardForm').on( "click", "#newCard", function() {
-        event.preventDefault();
-        getCard();
-      });
+	$('#cardForm').on('click', '#newCard', function() {
+		event.preventDefault();
+		getCard();
+	});
 
-    $('#cardForm').on( "click", "#btnRules", function() {
-        event.preventDefault();
-        $('#rules').toggleClass('hidden');
-      });
-   
-  }
-
+	$('#cardForm').on('click', '#btnRules', function() {
+		event.preventDefault();
+		$('#rules').toggleClass('hidden');
+	});
+}
 
 // Items to run in the beginning of the page load
 $(function() {
-    buttons();
-    shuffle(STORE);
-  });
+	buttons();
+	shuffle(STORE);
+});
